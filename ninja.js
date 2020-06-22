252af50985a078de91cda4d670cd4c5c0555ea43
Tanner Smith <tanner.ssu@gmail.com>
2020-06-22 10:23:53 -0500

    finished super ninja

--- ninja.js
+++ ninja.js
@@ -20,8 +20,28 @@ class Ninja {
 
 }
 
+class Sensei extends Ninja {
+    constructor(name) {
+        super()
+        this.name = name;
+        this.health = 200;
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+
+    speakWisdom() {
+        super.drinkSake()
+        console.log('What one programmer can do in one month, two programmers can do in two months.')
+    }
+}
+
 const ninja1 = new Ninja("Tanner")
-ninja1.sayName()
-ninja1.showStats()
-ninja1.drinkSake()
-ninja1.showStats()
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
+ninja1.showStats();
+
+const superSensei = new Sensei("Master Splinter")
+superSensei.speakWisdom();
+superSensei.showStats();
